2ed901474df09ba5624c4147b264ff14ea005db9
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-07 15:18:24 -0400

    refactor

--- 01-reforzamiento/src/components/Usuarios.tsx
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -1,31 +1,9 @@
-import { useEffect, useRef, useState } from "react"
-import { reqResApi } from "../api/reqRes"
-import { ReqResListado, Usuario } from "../interfaces/reqRes"
+import useUsuarios from "../hooks/useUsuarios";
+import { Usuario } from "../interfaces/reqRes"
 
 const Usuarios = () => {
 
-  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
-  
-  const paginaRef = useRef(1);
-
-  useEffect(() => {
-    cargarUsuarios();
-  }, [])
-
-  const cargarUsuarios = async () => {
-    const res = await reqResApi.get< ReqResListado >('/users', {
-      params: {
-        page: paginaRef.current
-      }
-    });
-
-    if( res.data.data.length > 0 ) {
-      setUsuarios( res.data.data )
-      paginaRef.current++;
-    } else {
-      alert('No hay mas registros')
-    }
-  }
+  const { usuarios, cargarUsuarios } = useUsuarios();
 
   const renderItems = ( usuario: Usuario ) => {
     const { id, avatar, first_name, last_name, email } = usuario;
