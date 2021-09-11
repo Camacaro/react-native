606c5211ed007f97b49d02ed0ecb12cfab3b4ae5
unknown <jesus.alejandr@gmail.com>
2021-09-11 17:57:00 -0300

    Modal

---
+++ 06-RNComponents/src/screens/ModalScreen.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {View, Text, Button, Modal} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
+
+export const ModalScreen = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <View style={styles.globalMargin}>
+      <HeaderTitle title="Modal" />
+
+      <Button title="Abrir Modal" onPress={() => setIsVisible(true)} />
+
+      <Modal animationType="fade" visible={isVisible} transparent>
+        {/* Fondo */}
+        <View
+          style={{
+            flex: 1,
+            // Combinado con transparent en Modal para verl el fondo y ocultar el flex
+            // height: 100,
+            // width: 100,
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {/* Contenido del modal */}
+          <View
+            style={{
+              backgroundColor: 'white',
+              width: 200,
+              height: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowOffset: {
+                width: 0,
+                height: 0,
+              },
+              shadowOpacity: 0.25,
+              elevation: 10,
+              borderRadius: 5,
+            }}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Titulo modal</Text>
+            <Text style={{fontSize: 16, fontWeight: '300', marginBottom: 20}}>
+              Cuerpo del modal
+            </Text>
+            <Button title="Cerrar" onPress={() => setIsVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
