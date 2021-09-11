1ac0d2f5c2f87ff70a1d932f3a54cdd604db72c5
unknown <jesus.alejandr@gmail.com>
2021-09-11 15:10:21 -0300

    promt android

--- 06-RNComponents/src/screens/AlertScreen.tsx
+++ 06-RNComponents/src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Button, Alert} from 'react-native';
+import prompt from 'react-native-prompt-android';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
@@ -23,10 +24,55 @@ export const AlertScreen = () => {
     );
   };
 
+  // Esto solo sirve en IOS
+  const showPropmtIOS = () => {
+    Alert.prompt(
+      '¿Esta seguro?',
+      'Esta accion no se puede revertir',
+      (value: string) => console.log('Propmt value: ', value),
+      'login-password',
+      'Valor por defecto',
+      'number-pad',
+    );
+  };
+
+  const showPropmt = () => {
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
+    );
+  };
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
       <Button title="Mostrar Alerta" onPress={showAlert} />
+
+      <View style={{height: 10}} />
+
+      {/* Solo sirve en IOS */}
+      <Button title="Mostrar Prompt" onPress={showPropmt} />
+
+      <View style={{height: 10}} />
+
+      <Button title="Mostrar Prompt (IOS)" onPress={showPropmtIOS} />
     </View>
   );
 };
